70ae7a64367fef5c3fe0a387beaa2035edcfddad
Leon Matthes <leon@matthes.biz>
2025-01-25 14:48:13 +0100

    Player Select Menu

--- scripts/globals.gd
+++ scripts/globals.gd
@@ -1,3 +1,6 @@
 extends Node
 
 var is_player_1 := true
+
+func load_level(level: PackedScene):
+	get_tree().change_scene_to_packed(level)
--- scripts/player_select.gd
+++ scripts/player_select.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
-
+var next_level := preload("res://Menus/PlayerInstructions.tscn")
 
 func _on_player_1_selected() -> void:
 	Globals.is_player_1 = true
+	get_tree().change_scene_to_packed(next_level)
 
 func _on_player_2_selected() -> void:
 	Globals.is_player_1 = false
+	get_tree().change_scene_to_packed(next_level)
