eea272acc3452fec46415e2ee3f13887efb0b94a
Leon Matthes <leon@matthes.biz>
2025-01-26 00:07:11 +0100

    Fix end

--- level_end_trigger.gd
+++ level_end_trigger.gd
@@ -2,22 +2,12 @@ extends Area2D
 
 @export var next_level_alice: String
 @export var next_level_bob: String
-var exit_ready
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	exit_ready = false
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_key_pressed(Key.KEY_ENTER) && exit_ready:
-		var next_level = next_level_alice if Globals.is_player_1 else next_level_bob
-		get_tree().change_scene_to_packed(load(next_level))
-
-
-func _on_body_entered(body: Node2D) -> void:
-	exit_ready = true
-
-func _on_body_exited(body: Node2D) -> void:
-	exit_ready = false
+	if Input.is_key_pressed(Key.KEY_ENTER):
+		for body in get_overlapping_bodies():
+			if body.is_in_group("players"):
+				var next_level = next_level_alice if Globals.is_player_1 else next_level_bob
+				get_tree().change_scene_to_packed(load(next_level))
+				break
